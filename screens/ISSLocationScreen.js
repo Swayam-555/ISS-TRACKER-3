813a1bbdcdb0f01d5f279ff3eacc34dbce0c738c
jncharul <74925396+jncharul@users.noreply.github.com>
2022-04-15 21:14:13 +0530

    Update ISSLocationScreen.js

--- screens/ISSLocationScreen.js
+++ screens/ISSLocationScreen.js
@@ -1,17 +1,19 @@
-import * as React from 'react';
-import { Text, View, ImageBackground, SafeAreaView, Alert, Image } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
 import axios from 'axios';
-
+import * as React from 'react';
+import { Text, View, SafeAreaView, ImageBackground, Image } from 'react-native';
+import MapView, { Marker } from "react-native-maps"
 
 export default class ISSLocationScreen extends React.Component {
     constructor() {
-        super()
+        super();
         this.state = {
             location: {}
         }
     }
-    getISSLocation = () => {
+    componentDidMount() {
+        this.getIssLocation()
+    }
+    getIssLocation = () => {
         axios
             .get("https://api.wheretheiss.at/v1/satellites/25544.")
             .then(response => {
@@ -22,15 +24,14 @@ export default class ISSLocationScreen extends React.Component {
             .catch(error => {
                 Alert.alert(error.message)
             })
-    }
-    componentDidMount() {
-        this.getISSLocation()
+
+
     }
     render() {
         if (Object.keys(this.state.location).length === 0) {
             return (
                 <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-                    <Text style={{ fontSize: 25 }}>Loading....</Text>
+                    <Text style={{ fontSize: 20 }}>{"Loading...."}</Text>
                 </View>
             )
         }
@@ -42,31 +43,40 @@ export default class ISSLocationScreen extends React.Component {
                     <SafeAreaView style={{ flex: 1 }}>
                         <ImageBackground source={require("../assets/iss_bg.jpg")}
                             style={{ flex: 1, resizeMode: "cover" }}>
-                            <View style={{ flex: 0.1, justifyContent: "center", alignItems: "center" }}>
-                                <Text style={{ color: "white", fontWeight: "bold", fontSize: 25 }}>ISS Location</Text>
+                            <View style={{ flex: 0.1, alignItems: "center", justifyContent: "center" }}>
+                                <Text style={{ color: "white", fontWeight: "bold" }}>
+                                    ISS Location
+                </Text >
                             </View>
                             <View style={{ flex: 0.7 }}>
                                 <MapView region={{
                                     latitude: this.state.location.latitude,
                                     longitude: this.state.location.longitude,
-                                    longitudeDelta: 100,
-                                    latitudeDelta: 100
+                                    latitudeDelta: 100,
+                                    longitudeDelta: 100
                                 }}
                                     style={{ width: "100%", height: "100%" }}>
                                     <Marker coordinate={{
-                                        latitude: this.state.location.latitude,
-                                        longitude: this.state.location.longitude
+                                        longitude: this.state.location.longitude,
+                                        latitude: this.state.location.latitude
                                     }}>
-                                        <Image source={require("../assets/iss_icon.png")}
-                                            style={{ height: 50, width: 50 }} />
+                                        <Image source={require("../assets/iss_icon.png")} style={{ height: 100, width: 100 }} />
                                     </Marker>
-
                                 </MapView>
+
+                            </View>
+                            <View style={{ flex: 0.2, marginTop: -10, backgroundColor: "white", padding: 30, borderRadius: 30 }}>
+                                <Text>longitude : {this.state.location.longitude}</Text>
+                                <Text>latitude : {this.state.location.latitude}</Text>
+                                <Text>Altitude : {this.state.location.altitude}</Text>
+                                <Text>Velocity : {this.state.location.velocity}</Text>
                             </View>
                         </ImageBackground>
                     </SafeAreaView>
+
                 </View>
             )
         }
     }
+
 }
